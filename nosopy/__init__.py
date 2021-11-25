c8ef18def0f0ff6141593350a0af95fb940e5f9e
Gustavo Carreno <guscarreno@gmail.com>
2021-11-25 14:36:06 +0000

    chore: Adding long_description=README

--- nosopy/__init__.py
+++ nosopy/__init__.py
@@ -1,5 +1,7 @@
 import socket
 
+__version__ = '0.4.3'
+
 """
   NosoSocket
 
--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
+import pathlib
 from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
 
 setup(
     name='nosopy',
-    version='0.3.1',
+    version='0.4.3',
     description='Set of classes and tools to communicate with a Noso wallet using NosoP',
     url='https://github.com/Noso-Project/NosoPy',
     license='Unlicense',
     author="Gustavo 'Gus' Carreno",
     author_email='guscarreno@gmail.com',
-    packages=find_packages()
+    packages=find_packages(exclude=("tests")),
+    long_description=README,
+    long_description_content_type="text/markdown"
 )
