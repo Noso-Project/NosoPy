6e4dd16cdb6349014d12cd795d10f689ba8d2d3e
Gustavo Carreno <guscarreno@gmail.com>
2021-11-25 12:23:43 +0000

    refactor: Finally the folder strcuture I wanted

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='nosopy',
     version='0.3.1',
     description='Set of classes and tools to communicate with a Noso wallet using NosoP',
+    url='https://github.com/Noso-Project/NosoPy',
+    license='Unlicense',
     author="Gustavo 'Gus' Carreno",
     author_email='guscarreno@gmail.com',
-    packages=['nosopy']
+    packages=find_packages()
 )
